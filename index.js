837cf1b487f034f8bf3c97ca4e1564ec1dd2f701
kovacste <kovacste@bazisnet.hu>
2020-07-31 15:19:24 +0200

    inital commit

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const urlToMp3 = require('./converter.js');
-const removeJob = require('./fileRemoveJob')
 var compression = require('compression');
-
-var Ddos = require('ddos');
-//var ddos = new Ddos({burst:10, limit:15})
 
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname+'./public'));
+app.use(express.static('public'));
 app.use(compression());
-//app.use(ddos.express);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname+'/about.html')));
@@ -23,7 +18,6 @@ app.get('/contact', (req, res) => res.sendFile(path.join(__dirname+'/contact.htm
 app.get('/termsofservices', (req, res) => res.sendFile(path.join(__dirname+'/termsofservices.html')));
 app.get('/privacypolicy', (req, res) => res.sendFile(path.join(__dirname+'/privacypolicy.html')));
 app.get('*', (req, res) => res.sendFile(path.join(__dirname+'/notfound.html')));
-
 
 
 app.post('/getYTMusic', (req, res) => {
@@ -43,13 +37,6 @@ app.post('/getYTMusic', (req, res) => {
         }
     }
 });
-/*
 
-var cron = require('node-cron');
-cron.schedule('1 * * * * *', () => {
-    removeJob.removeFiles();
-    console.log('Jobs done')
-})
-*/
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
